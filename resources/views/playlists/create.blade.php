ad9b21da2352a2dba4162d6d890f890d6f4f7e0c
Michael Avagianos <hello@maikkun.dev>
2025-08-17 16:40:00 +0300

    fix: remove deprecated attribute

--- resources/views/playlists/create.blade.php
+++ resources/views/playlists/create.blade.php
@@ -10,7 +10,7 @@
                 <div class="d-flex align-items-center mb-3">
                     <a href="{{ route('playlists.index') }}" class="text-muted me-3 text-decoration-none"
                        style="color: #706f6c !important;">
-                        <svg width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <svg width="24" height="24" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7">
                             </path>
                         </svg>
